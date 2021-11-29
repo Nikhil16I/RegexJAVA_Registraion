395ebd6d70aa502ba13c53aa1ff6588c1ed21834
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-11-29 21:58:51 +0530

    Adding First Name '1st Uppercase' in Username

---
+++ MainClass.java
@@ -0,0 +1,16 @@
+package RegexJAVA;
+
+import java.util.Scanner;
+
+public class MainClass {
+	public static void main(String[] args) {
+		RegexRegistration regex = new RegexRegistration();
+
+		Scanner name = new Scanner(System.in);
+		System.out.println("Input First Name :- ");
+		
+		String firstname = name.nextLine();
+		regex.ValidateFirstName(firstname);
+		
+	}
+}
---
+++ RegexRegistration.java
@@ -0,0 +1,18 @@
+package RegexJAVA;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexRegistration {
+
+	
+	public static  boolean ValidateFirstName(String firstName) {
+		String UserFirstName= "^[A-Z][a-z]{3,10}$";
+	
+		Pattern pattern =Pattern.compile(UserFirstName);
+		Matcher matcher = pattern.matcher(firstName);
+		
+	System.out.println(matcher.matches());
+		return matcher.matches();
+	}
+}
